b7f283fe6e3e7afc2f51373f33b132adb854e2bf
tjgrist <tjgrst@gmail.com>
2022-03-15 09:20:00 -0700

    uppercase jpg

--- src/images.js
+++ src/images.js
@@ -5,34 +5,58 @@ or right-clicking and then 'Get Info' on Mac
 */
 
 const slideImages = [
-    {
-      url: "1.jpg",
-      caption: "Image 1",
-      width: 5456,
-      height: 3632,
-    },
-    {
-      url: "2.jpg",
-      caption: "Image 2",
-      width: 2288,
-      height: 1712,
-    },
-    {
-      url: "3.jpg",
-      caption: "Image 3",
-      width: 2288,
-      height: 1712,
-    },
-    {
-      url: "4.jpg",
-      caption: "Image 4",
-      width: 2288,
-      height: 1712,
-    },
+  {
+    url: "1.JPG",
+    caption: "Image 1",
+    width: 5456,
+    height: 3632,
+  },
+  {
+    url: "2.JPG",
+    caption: "Image 2",
+    width: 2288,
+    height: 1712,
+  },
+  {
+    url: "3.JPG",
+    caption: "Image 3",
+    width: 2288,
+    height: 1712,
+  },
+  {
+    url: "4.JPG",
+    caption: "Image 4",
+    width: 2288,
+    height: 1712,
+  },
+  {
+    url: "1.jpg",
+    caption: "Image 1",
+    width: 5456,
+    height: 3632,
+  },
+  {
+    url: "2.jpg",
+    caption: "Image 2",
+    width: 2288,
+    height: 1712,
+  },
+  {
+    url: "3.jpg",
+    caption: "Image 3",
+    width: 2288,
+    height: 1712,
+  },
+  {
+    url: "4.jpg",
+    caption: "Image 4",
+    width: 2288,
+    height: 1712,
+  },
 
-    // ... add more images here by copying lines 8-31 above and pasting on line 32. 
-    // Add as many as you need, just be sure to edit the 'url', 'caption', 'width', and 'height' properties.
-    // You can rename or delete the examples above by deleting everything in the 'curly brackets'.
-  ];
+  // ... add more images here by copying lines 8-31 above and pasting on line 32.
+  // Add as many as you need, just be sure to edit the 'url', 'caption', 'width', and 'height' properties.
+  // You can rename or delete the examples above by deleting everything in the 'curly brackets'.
+];
 
 export default slideImages;
