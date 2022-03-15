5fea8bcfeb0e02522741389f475ae93a8c5abda6
tjgrist <tjgrst@gmail.com>
2022-03-15 14:56:24 -0700

    update favicon

--- pages/index.js
+++ pages/index.js
@@ -11,6 +11,9 @@ export default function Home() {
         <title>Meegan Farrell Art</title>
         <meta name="description" content="Meegan Farrell Art Portfolio" />
         <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
       </Head>
 
       <main>
