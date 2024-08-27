4ba97f9c57c395d5a82e190c0a2606ce79ad3954
tj.gristalaskaair.com <tj.grist@alaskaair.com>
2024-08-27 16:15:40 -0700

    update base img

--- app/page.jsx
+++ app/page.jsx
@@ -1,30 +1,16 @@
-// import Gallery from "./components/slider"
 import imageData from "./lib/images"
 import Image from "next/image"
 
-// async function getInitialProcessedImages() {
-//   const initialProcessedImages = imageData.map((i, idx) => ({
-//     ...i,
-//     thumbnail: i.original,
-//     originalAlt: `Meegan Farrell original art piece ${idx + 1}`,
-//     thumbnailAlt: `Meegan Farrell original art piece ${idx + 1}`,
-//     originalTitle: i.title,
-//     loading: idx === 0 ? "eager" : "lazy",
-//     thumbnailLoading: idx === 0 ? "eager" : "lazy",
-//   }))
-
-//   return initialProcessedImages
-// }
-
 export default async function Home() {
-//   const initialProcessedImages = await getInitialProcessedImages()
+  const imageName = process.env.NEXT_PUBLIC_IMAGE_NAME
 
-  const firstImage = imageData.find((i) => i.title === "DSC00408")
+  const randomImage = imageData[Math.floor(Math.random() * imageData.length)]
+
+  const firstImage = imageData.find(i => i.title === imageName || randomImage );
 
   return (
     <main className="">
         <Image priority={true} src={firstImage.original} alt={firstImage.title} width={firstImage.width} height={firstImage.height} />
-      {/* <Gallery initialProcessedImages={initialProcessedImages} /> */}
     </main>
   )
 }
