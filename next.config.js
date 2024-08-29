c88eab48b30c999357e67298a47dd5af202fb8cd
tj.gristalaskaair.com <tj.grist@alaskaair.com>
2024-08-29 15:34:52 -0700

    throw if no req env var

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
+
+const validateEnv = (requiredEnvVars) => {
+  for (const envVar of requiredEnvVars) {
+    if (!process.env[envVar]) {
+      throw new Error(`Environment variable ${envVar} is missing`);
+    }
+  }
+};
+
+const requiredEnvVars = ['S3_REGION', 'AWS_ACCESS_KEY_ID', 'AWS_ACCESS_KEY_SECRET', 'S3_BUCKET_NAME'];
+
 const nextConfig = {
   reactStrictMode: true,
   images: {
@@ -12,6 +23,17 @@ const nextConfig = {
       }
     ],
   },
+  env: {
+    NEXT_PUBLIC_GA_MEASUREMENT_ID: "G-41ZW7BWZXY",
+    NEXT_PUBLIC_IMAGE_NAME: 'DSC00355',
+    S3_REGION: process.env.S3_REGION || new Error('S3_REGION is required'),
+    AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID || new Error('AWS_ACCESS_KEY_ID is required'),
+    AWS_ACCESS_KEY_SECRET: process.env.AWS_ACCESS_KEY_SECRET || new Error('AWS_ACCESS_KEY_SECRET is required'),
+    S3_BUCKET_NAME: process.env.S3_BUCKET_NAME || new Error('S3_BUCKET_NAME is required'),
+  },
 }
 
-module.exports = nextConfig
+module.exports = () =>{
+  validateEnv(requiredEnvVars);
+  return nextConfig;
+}
